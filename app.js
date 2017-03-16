87b14132797db2303ffb2e9c81c9cefc28b38415
LKLP66 <krasotkina11@gmail.com>
2017-03-16 22:53:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var contacts = require('./routes/contacts');
 
 var app = express();
 
-// view engine setup
+// view engine setup//
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
